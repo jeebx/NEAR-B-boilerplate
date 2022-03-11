5f16455b38945b4a99a2e283bd67a2101b36bd41
jeebx <jeeb24@gmail.com>
2022-03-11 12:51:45 -0700

    generalize header

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
     <>
         <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Protonauts About</title>
+        <title>New Web3 App</title>
         </Head>
         <Layout>
             <Component {...pageProps} />
